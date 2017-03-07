4aa755543e7c426c51e5908a0734b7fb5886f56a
Kyle Johnson <kyle@solidstategroup.com>
2017-03-07 22:49:40 +0000

    Update index.js

--- rio-server/index.js
+++ rio-server/index.js
@@ -39,12 +39,5 @@ wsClient.init(app);
 const getImageData = require('./inputs/gif-url-input');
 const getVideoData = require('./inputs/video-url-input');
 
-getImageData('https://media.giphy.com/media/3o7buekWPtAWpscQCY/giphy.gif'); // 50fps 50x30 RGB rect
-//getImageData('http://i.giphy.com/3oz8xEMwRxFQV21ntC.gif'); // 5s 6x5 stripey GIF
-// getImageData('http://i.giphy.com/l0MYE0GTUJFY5PWcU.gif'); // 30x1 5s 2 frame GIF
-// getImageData('http://i.giphy.com/3oz8xwDZ6N4vt2D6so.gif'); // 5s 12x10 stripey GIF
-
-//getVideoData('https://www.youtube.com/watch?v=wS8ZC271eMQ');
-
-
+getImageData('https://media.giphy.com/media/3o7buekWPtAWpscQCY/giphy.gif'); 
 module.exports = app;
