c9c6738d960075e11be2741e3ecee14ffd06c6b4
Luke Fanning <luke@solidstategroup.com>
2017-05-15 16:04:34 +0100

    Adds GIF URL endpoint

--- rio-server/clients/web-client/index.js
+++ rio-server/clients/web-client/index.js
@@ -1,6 +1,7 @@
 var Router = require('express').Router;
 var messageAPI = require('./slack-api');
 var textAPI = require('./text-api');
+var gifAPI = require('./gif-api');
 
 module.exports = function () {
     var api = Router();
@@ -10,8 +11,7 @@ module.exports = function () {
         res.send('Her name is rio and she dances on the sand')
     });
     api.use('/text', textAPI());
-
-
+    api.use('/gif', gifAPI());
 
     return api;
 };
